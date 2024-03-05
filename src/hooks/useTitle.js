2dd3bb930a6c6b3311a3cce7ac5039bc2bbcf607
P PADMAPRASAD SHENOY <ppadmaprasadshenoy@gmail.com>
2024-03-05 16:20:46 +0530

    first commit

---
+++ src/hooks/useTitle.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+export const useTitle = (title) => {
+
+  useEffect(() => {
+    document.title = `${title} | Shopping Cart`
+  }, [title])
+  return (
+    <div>useTitle</div>
+  )
+}
